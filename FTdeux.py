1a3be6b0c009baaffc65e696e8b6f518c0cd5eb1
youss <youssef.elbali@gmail.com>
2024-04-24 15:59:10 +0200

    FTdeux is the first facetracking who work but with some default . The face recognition is too slow, same thing for the live windows to pop.

--- FTdeux.py
+++ FTdeux.py
@@ -3,8 +3,7 @@ import numpy as np
 from djitellopy import tello
 import time
 
-
-me=tello.Tello()
+me = tello.Tello()
 me.connect()
 print(me.get_battery())
 
@@ -18,13 +17,10 @@ me.takeoff()
 me.send_rc_control(0,0,25,0)
 time.sleep(2.2)
 
-
-
 fbRange = [6200,6800]
 pid = [0.4, 0.4, 0]
 pError = 0
 w,h = 360, 240
-
 
 def findFace(img):
     faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
@@ -36,7 +32,6 @@ def findFace(img):
 
     for (x,y,w,h) in faces:
         cv2.rectangle(img, (x, y),(x + w, y + h),(0,0,255), 2)
-# Centrer le carré du visage ?
         cx = x + w // 2
         cy = y + h //2
         area = w * h
@@ -51,54 +46,37 @@ def findFace(img):
         return img, [[0,0], 0]
 
 
-#fonction de suivi
 def trackFace(me, info, w, pid, pError):
     area = info[1]
     x,y = info[0]
     fb = 0
 
-#calcul du delta avec le centre pour recalibré
     error = x - w//2
     speed = pid[0] * error + pid[1] * (error-pError)
     speed = int(np.clip(speed, -100, 100))
 
-    #vert zone
     if area > fbRange[0] and area < fbRange[1]:
         fb=0
-    #trop pres
-    if area>fbRange[1]:
+    elif area>fbRange[1]:
         fb=-20
-    #trop loin
     elif area<fbRange[0] and area != 0:
         fb=20
-
-
 
     if x == 0:
         speed=0
         error=0
 
-
     me.send_rc_control(0, fb, 0, speed)
 
     return error
 
-
-
-#cap = cv2.VideoCapture(0)
 while True:
-    #_, img = cap.read()
-
     img = me.get_frame_read().frame
-
     # Appliquer un filtre de correction de couleur pour corriger la couleur bleu
-    #img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = cv2.resize(img, (w, h))
     img, info = findFace(img)
-    pError = trackFace( info, w, pid, pError)
-    #print("Aire de reconnaissance",info[1],"Centre",info[1])
-
+    pError = trackFace(me, info, w, pid, pError)
     cv2.imshow("Output",img)
 
     if cv2.waitKey(1) & 0xff == ord('k'):
